9a43612451a30b3137dcb29ab2c45d277c49030c
sigflup <pantsbutt@gmail.com>
2012-08-10 17:22:06 -0500

    we didn't need autom4te.cache

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 make clean
+rm -rf autom4te.cache
 rm -rf configure config.h.in config.log config.h Makefile.windows Makefile config.status gui/Makefile gui/Makefile.windows
 autoconf
 autoheader
