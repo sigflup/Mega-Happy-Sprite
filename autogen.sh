22a8623cfa57ab943d0a4e684e156e085d27862d
sigflup <sigflup@obsd76desk.sigflup>
2024-11-19 12:46:47 -0600

    no autoconf for windows

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-make clean
+export AUTOCONF_VERSION=2.69
+
+gmake clean
 rm -rf autom4te.cache
 rm -rf configure config.h.in config.log config.h Makefile.windows Makefile config.status gui/Makefile gui/Makefile.windows
 autoconf
